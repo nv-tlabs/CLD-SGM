3bc5f6593cec5751fb3dc7657ee1f51d5c8df8e6
timudk <timdockhorn@gmail.com>
2022-06-06 17:30:04 -0400

    Additing toy resnet architecture

--- models/mlp.py
+++ models/mlp.py
@@ -46,3 +46,48 @@ class MLP(nn.Module):
         output = self.main(h)
 
         return output
+
+@utils.register_model(name='resnet')
+class ResNet(nn.Module):
+    def __init__(self,
+                 config,
+                 input_dim=2,
+                 index_dim=1,
+                 hidden_dim=64,
+                 n_hidden_layers=20):
+
+        super().__init__()
+
+        self.act = nn.SiLU()
+        self.n_hidden_layers = n_hidden_layers
+
+        self.x_input = True
+        self.z_input = True if config.sde == 'cld' else False
+
+        if self.x_input and self.z_input:
+            in_dim = input_dim * 2 + index_dim
+        else:
+            in_dim = input_dim + index_dim
+        out_dim = input_dim
+
+        layers = []
+        layers.append(nn.Linear(in_dim, hidden_dim))
+        for _ in range(n_hidden_layers):
+            layers.append(nn.Linear(hidden_dim + index_dim, hidden_dim))
+        layers.append(nn.Linear(hidden_dim + index_dim, out_dim))
+
+        self.layers = nn.ModuleList(layers)
+
+    def _append_time(self, h, t):
+        time_embedding = torch.log(t)
+        return torch.cat([h, time_embedding.reshape(-1, 1)], dim=1)
+
+    def forward(self, u, t):
+        h0 = self.layers[0](self._append_time(u, t))
+        h = self.act(h0)
+
+        for i in range(self.n_hidden_layers):
+            h_new = self.layers[i + 1](self._append_time(h, t))
+            h = self.act(h + h_new)
+
+        return self.layers[-1](self._append_time(h, t))
